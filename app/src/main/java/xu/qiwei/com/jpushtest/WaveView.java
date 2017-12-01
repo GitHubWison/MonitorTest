b5ed88d0bf04fec70e5e2d89dd9479b0b68e1eff
GitHubWison <15995448871@163.com>
2017-12-01 09:43:58 +0800

    心电图动画效果：刷新条可配置

--- app/src/main/java/xu/qiwei/com/jpushtest/WaveView.java
+++ app/src/main/java/xu/qiwei/com/jpushtest/WaveView.java
@@ -5,13 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PaintFlagsDrawFilter;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -34,13 +34,16 @@ public class WaveView extends View implements WaveFormInterface {
     private int count = 0;
     private List<WaveFormBean> datas = new ArrayList<>();
     private List<WaveFormBean> newDatas = new ArrayList<>();
-    private int refreshstartx = 0;
-    private int refreshstarty = 0;
-    private int refreshendx = 0;
-    private int refreshendy = 0;
+//    private int refreshstartx = 0;
+//    private int refreshstarty = 0;
+//    private int refreshendx = 0;
+//    private int refreshendy = 0;
 
     private int strokeOne = 100;
     private int strokeTwo = 150;
+    private List<WaveFormBean> spaceWave = new ArrayList<>();
+    private Rect refreshRect = new Rect(0,0,0,0);
+    private static final int REFRESH_WIDTH = 20;
 //    private UpdateUIHandler updateUIHandler;
 
 
@@ -55,32 +58,17 @@ public class WaveView extends View implements WaveFormInterface {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        this.canVas = canvas;
         drawWave(canvas);
-//        drawPathWave(canvas);
+//        drawTest(canvas);
     }
 
-    private void drawPathWave(Canvas canvas) {
-        //初始化Paint
-        Paint paint = new Paint();
+    private void drawTest(Canvas canvas) {
+        paint = new Paint();
         paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10f);
-        canvas.drawLine(0,0,200,200,paint);
-        paint.setColor(Color.YELLOW);
-        canvas.drawLine(strokeTwo,strokeTwo,strokeOne,strokeOne,paint);
 
-////初始化Path
-//        Path path = new Path();
-////将坐标系原点从（0,0）移动到（100,100）
-////        path.moveTo(100, 100);
-////画从（100,100）到（400,400）之间的直线
-//        path.lineTo(400, 400);
-////path.rMoveTo(0, 100); //暂时注释
-//        path.lineTo(400, 800);
-//        canVas.drawPath(path, paint);
-
+        canvas.drawRect(refreshRect,paint);
     }
+
     public void refreshStroke(){
         strokeOne +=10;
         strokeTwo+=10;
@@ -97,7 +85,6 @@ public class WaveView extends View implements WaveFormInterface {
         canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG));
         paint = new Paint();
         paint.setStrokeWidth(5);
-//        paint.setColor(Color.BLUE);
         if (datas.size() != 0) {
             for (int listIndex = 0; listIndex < datas.size(); listIndex++) {
                 //////////////v2////////////////////
@@ -105,55 +92,24 @@ public class WaveView extends View implements WaveFormInterface {
                 int indexTemp_0 = listIndex - 2;//第一个点
                 int indexTemp_1 = listIndex - 1;//第二个点
                 int indexTemp_2 = listIndex;//第三个点
-
-
-/*                if (indexTemp_0 > 0) {
-//                    表示已经获得了三个以上的点
-                    WaveFormBean temp_0 = datas.get(indexTemp_0);
-                    WaveFormBean temp_1 = datas.get(indexTemp_1);
-                    WaveFormBean temp_2 = datas.get(indexTemp_2);
-                    if (indexTemp_2 == count) {
-//                        表示当前绘制的点位于刷新点
-//                        Log.e("warn-===","表示当前绘制的点位于刷新点");
-                        paint.setColor(Color.YELLOW);
-                    } else {
-//                        表示当前绘制的点不在心电的刷新点
-//                        Log.e("warn-===","表示当前绘制的点不在心电的刷新点");
-                        paint.setColor(Color.RED);
-                    }
-//                    开始绘制第二第三个点之间的连线
-                    canvas.drawLine(temp_1.getX(), BASE_LINE - getWaveY(temp_1.getY()) * TIMS, temp_2.getX(),
-                            BASE_LINE - getWaveY(temp_2.getY()) * TIMS, paint);
-//                     开始绘制第一第二个点之间的连线
-                    paint.setColor(Color.BLUE);
-                    canvas.drawLine(temp_0.getX(), BASE_LINE - getWaveY(temp_0.getY()) * TIMS, temp_1.getX(),
-                            BASE_LINE - getWaveY(temp_1.getY()) * TIMS, paint);
-
-                } else
-                {*/
-
-
 //                    表示已经获得了三个以下的点
                     if (indexTemp_1 > 0) {
-                        paint.setColor(Color.YELLOW);
-                        canvas.drawLine(refreshstartx,refreshstarty,refreshendx,refreshendy,paint);
+
+//                        canvas.drawLine(refreshstartx,refreshstarty,refreshendx,refreshendy,paint);
 //                        表示已经获得了两个点
                         WaveFormBean temp_1 = datas.get(indexTemp_1);
                         WaveFormBean temp_2 = datas.get(indexTemp_2);
-
-
 //                        连接第二第三个点
-                        paint.setColor(Color.BLUE);
-                        canvas.drawLine(temp_1.getX(), BASE_LINE - getWaveY(temp_1.getY()) * TIMS, temp_2.getX(),
-                                BASE_LINE - getWaveY(temp_2.getY()) * TIMS, paint);
+                        paint.setColor(Color.YELLOW);
+                        canvas.drawLine(temp_1.getX(), temp_1.getY() , temp_2.getX(),temp_2.getY(), paint);
+
+                        paint.setColor(Color.GRAY);
+                        canvas.drawRect(refreshRect,paint);
 
                     }else
                     {
 //                        只有一个点
                     }
-//                }
-
-                Log.e("","");
 
             }
         }
@@ -192,19 +148,14 @@ public class WaveView extends View implements WaveFormInterface {
             count = 0;
             int singleWidth = getWidth() / list.size();
             while (count < list.size()) {
-
-
-
                 WaveFormBean waveFormBean = list.get(count);
                 waveFormBean.setX(count * singleWidth);
+                waveFormBean.setY(BASE_LINE - getWaveY(waveFormBean.getY())* TIMS);
 
                 if (count-1>=0) {
                     WaveFormBean temp_1 = list.get(count-1);
                     WaveFormBean temp_2 = list.get(count);
-                    refreshstartx = temp_1.getX();
-                    refreshstarty = BASE_LINE - getWaveY(temp_1.getY())* TIMS;
-                    refreshendx = temp_2.getX();
-                    refreshendy = BASE_LINE - getWaveY(temp_2.getY())* TIMS;
+                    refreshRect.set(temp_1.getX(),0,temp_1.getX()+REFRESH_WIDTH, getHeight());
                 }
 
                 if (datas.size() - 1 < count) {
@@ -213,12 +164,6 @@ public class WaveView extends View implements WaveFormInterface {
                 } else {
 //                    代表之前已经绘制过了心电图,需要更新心电数据
                     datas.set(count, waveFormBean);
-//                    if (count==2) {
-//                        BASE_LINE = 1500;
-//                    }
-//                    if (count==3) {
-//                        return;
-//                    }
                 }
 
                 WaveView.this.post(new Runnable() {
@@ -235,7 +180,6 @@ public class WaveView extends View implements WaveFormInterface {
                 }
                 count++;
             }
-//            datas.clear();
 
         }
 
